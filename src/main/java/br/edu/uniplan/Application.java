cd11cd979f6420657b7a157cf08d46244159fb28
Erica Faria <fariaerica064@gmail.com>
2025-09-02 13:39:00 -0300

    Foi criado a tela de cadastro com o campo nome e botão cadastrar

--- src/main/java/br/edu/uniplan/Application.java
+++ src/main/java/br/edu/uniplan/Application.java
@@ -5,6 +5,13 @@
 
 package br.edu.uniplan;
 
+import br.edu.uniplan.projeto.banco.de.dados.domain.data.model.Usuario;
+import br.edu.uniplan.projeto.banco.de.dados.domain.service.CadastroService;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author Usuario
@@ -12,6 +19,41 @@ package br.edu.uniplan;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        CadastroService cadastroService = new CadastroService();
+
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(300, 200);
+        jFrame.setLayout(null);
+
+        JLabel jLabelNome = new JLabel("Nome:");
+        jLabelNome.setBounds(20, 20, 80, 25);
+        jFrame.add(jLabelNome);
+
+        JTextField jTextFieldNome = new JTextField();
+        jTextFieldNome.setBounds(80, 20, 150, 25);
+        jFrame.add(jTextFieldNome);
+
+        JButton jButton = new JButton("Cadastrar");
+        jButton.setBounds(80, 60, 100, 30);
+        jFrame.add(jButton);
+
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nome = jTextFieldNome.getText().trim();
+
+                Usuario usuario = new Usuario();
+                usuario.setCodigo(null);
+                usuario.setNome(nome);
+                usuario.setEndereco(null);
+
+                cadastroService.cadastrarUsuario(usuario);
+            }
+        });
+
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
     }
 }
